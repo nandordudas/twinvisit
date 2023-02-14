31afe5639d4a767cbd61633eee92555b8bb026ee
Nándor Dudás <nandor.dudas@gmail.com>
2023-02-14 14:33:43 +0000

    style: update design (APP-0)

--- apps/nuxt-app/pages/index.vue
+++ apps/nuxt-app/pages/index.vue
@@ -4,11 +4,11 @@ definePageMeta({
 })
 
 const fileNames: string[] = [
-  '/images/pexels-kate-trifo-3992003.webp',
-  '/images/pexels-carissa-rogers-1504236.webp',
-  '/images/pexels-proxyclick-visitor-management-system-2451567.webp',
-  '/images/pexels-max-vakhtbovych-6238608.webp',
-  '/images/pexels-brett-sayles-2475270.webp',
+  'https://twinvisit-nuxt-app.vercel.app/images/pexels-kate-trifo-3992003.webp',
+  'https://twinvisit-nuxt-app.vercel.app/images/pexels-carissa-rogers-1504236.webp',
+  'https://twinvisit-nuxt-app.vercel.app/images/pexels-proxyclick-visitor-management-system-2451567.webp',
+  'https://twinvisit-nuxt-app.vercel.app/images/pexels-max-vakhtbovych-6238608.webp',
+  'https://twinvisit-nuxt-app.vercel.app/images/pexels-brett-sayles-2475270.webp',
 ]
 
 const description = `
